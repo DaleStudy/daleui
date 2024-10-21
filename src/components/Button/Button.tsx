3087cafe2e5fce6b5b67561c3f4a1a6b20a08cf6
Dale Seo <dale.seo@gmail.com>
2024-10-20 20:48:34 -0400

    update button component to use panda css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import "./Button.css";
+import { css } from "../../../styled-system/css";
 
 export interface ButtonProps {
   children: React.ReactNode;
@@ -13,7 +13,38 @@ export const Button = ({
   ...rest
 }: ButtonProps) => {
   return (
-    <button type={type} onClick={onClick} {...rest}>
+    <button
+      className={css({
+        "--button-color": "#ffffff",
+        "--button-bg-color": "#0d6efd",
+        "--button-hover-bg-color": "#025ce2",
+        appearance: "none",
+        margin: "0",
+        padding: "0.5rem 1rem",
+        background: "var(--button-bg-color)",
+        color: "var(--button-color)",
+        fontSize: "1rem",
+        fontWeight: 400,
+        textAlign: "center",
+        textDecoration: "none",
+        display: "inline-block",
+        width: ["auto", "100%"],
+        border: "none",
+        borderRadius: "4px",
+        boxShadow:
+          "0 4px 6px -1px rgba(0, 0, 0, 0.1),\n    0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+        cursor: "pointer",
+        transition: "0.5s",
+        "&:active,\n  &:hover,\n  &:focus": {
+          background: "var(--button-hover-bg-color)",
+          outline: "0",
+        },
+        "&:disabled": { opacity: 0.5 },
+      })}
+      type={type}
+      onClick={onClick}
+      {...rest}
+    >
       {children}
     </button>
   );
