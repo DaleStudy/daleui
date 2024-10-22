196adcf3b88aa1b7a5a015603d45c9bba77e6835
Dale Seo <dale.seo@gmail.com>
2024-10-21 21:10:06 -0400

    css scoping

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,3 @@
-import "./Button.css";
-
 export interface ButtonProps {
   children: React.ReactNode;
   type?: "button" | "submit";
@@ -14,7 +12,52 @@ export const Button = ({
 }: ButtonProps) => {
   return (
     <button type={type} onClick={onClick} {...rest}>
+      <style>{style}</style>
       {children}
     </button>
   );
 };
+
+const style = `@scope {
+  & {
+    --button-color: #ffffff;
+    --button-bg-color: #0d6efd;
+    --button-hover-bg-color: #025ce2;
+
+    appearance: none;
+
+    margin: 0;
+    padding: 0.5rem 1rem;
+
+    background: var(--button-bg-color);
+    color: var(--button-color);
+
+    font-size: 1rem;
+    font-weight: 400;
+    text-align: center;
+    text-decoration: none;
+
+    display: inline-block;
+    width: auto;
+
+    border: none;
+    border-radius: 4px;
+
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    cursor: pointer;
+    transition: 0.5s;
+
+    width: 100%;
+
+    &:active,
+    &:hover,
+    &:focus {
+      background: var(--button-hover-bg-color);
+      outline: 0;
+    }
+    &:disabled {
+      opacity: 0.5;
+    }
+  }
+}`;
