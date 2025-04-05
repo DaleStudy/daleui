00cbf99ec1b05fd2953e5de48b3148457e79abde
SeonjaeLee <tjswo9502@gmail.com>
2025-04-05 21:58:42 +0900

    feature: link component design
    Co-authored-by: RiaOh <ken5840714@@gmail.com>
    Co-authored-by: hyoseong1994 <wjdgytjd3291@gmail.com>

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,18 @@
+import type { AnchorHTMLAttributes, PropsWithChildren } from "react";
+import type { Tone } from "../../tokens/colors";
+import type { FontSize, FontWeight } from "../../tokens/typography";
+
+interface LinkWithoutChildren extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  /** 색조 */
+  tone?: Tone;
+  /** 크기 */
+  size?: FontSize;
+  /** 굵기 */
+  weight?: FontWeight;
+  /** 명암비 낮출지 */
+  muted?: boolean;
+  /** 링크에 밑줄 표시 여부 */
+  underline?: boolean;
+}
+
+export type LinkProps = PropsWithChildren<LinkWithoutChildren>;
