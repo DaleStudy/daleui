b5c1aa4b7ca54d48d7c51964b123ae3063299f77
sounmind <devleesche@gmail.com>
2025-04-11 00:52:14 -0400

    feat: update RadioGroup design and tests

--- src/components/RadioGroup/RadioGroup.test.tsx
+++ src/components/RadioGroup/RadioGroup.test.tsx
@@ -1,33 +1,35 @@
 import { test } from "vitest";
 
-test.todo("options의 label과 value가 올바르게 렌더링됨");
+test.todo("RadioGroup이 올바르게 자식 요소와 함께 렌더링됨");
+test.todo(
+  "RadioGroup.Item이 RadioGroup.Root 외부에서 사용될 경우 에러가 발생함"
+);
+test.todo("최소 1개의 RadioGroup.Item이 필요함");
+test.todo("RadioGroup.Root의 name 속성이 올바르게 설정됨");
 
-test.todo("options의 disabled 속성이 올바르게 적용됨");
-
-test.todo("2개 미만 options 제공 시 에러가 발생함");
-
-test.todo("ReactNode 타입 label이 올바르게 렌더링됨");
-
-test.todo("name 속성이 모든 라디오 버튼에 올바르게 적용됨");
+test.todo("RadioGroup.Item의 value가 올바르게 동작함");
+test.todo("RadioGroup.Item의 disabled 속성이 올바르게 적용됨");
+test.todo("RadioGroup.Item 선택 시 onChange가 호출됨");
 
 test.todo("defaultValue가 제공될 때 해당 값이 선택됨");
-
 test.todo("defaultValue가 제공되지 않을 때 아무것도 선택되지 않음");
-
-test.todo("value가 제공될 때 해당 값이 선택됨 (제어 컴포넌트)");
-
 test.todo("value와 defaultValue가 모두 제공될 때 value가 우선시됨");
 
-test.todo("옵션 선택 시 onChange가 호출됨");
-
-test.todo("disabled가 true일 때 모든 라디오 버튼이 비활성화됨");
+test.todo("Root의 disabled가 true일 때 모든 RadioGroup.Item이 비활성화됨");
+test.todo("Item의 disabled가 true일 때 해당 아이템만 비활성화됨");
 
 test.todo("required가 true일 때 필수 입력으로 표시됨");
 
-test.todo("orientation이 'horizontal'일 때 라디오 버튼이 가로로 배열됨");
+test.todo("orientation이 'horizontal'일 때 RadioGroup.Item이 가로로 배열됨");
+test.todo("orientation이 'vertical'일 때 RadioGroup.Item이 세로로 배열됨");
 
-test.todo("orientation이 'vertical'일 때 라디오 버튼이 세로로 배열됨");
+test.todo("키보드 방향키로 RadioGroup.Item 간 이동이 가능함");
+test.todo("Tab 키로 RadioGroup에 초점을 맞출 수 있음");
 
-test.todo("키보드 방향키로 옵션 간 이동이 가능함");
+test.todo("폼 제출 시 선택된 RadioGroup 값이 올바르게 전송됨");
+test.todo("폼 리셋 시 RadioGroup이 defaultValue로 초기화됨");
+
+test.todo("RadioGroup.Item이 동적으로 추가/제거될 때 정상 작동함");
+test.todo("RadioGroup.Item 사이에 다른 컴포넌트가 있어도 올바르게 작동함");
 
 test.todo("tone prop에 따라 올바른 색상이 적용됨");
--- src/components/RadioGroup/RadioGroup.tsx
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -1,19 +1,12 @@
-import type { ReactNode } from "react";
+import { type ReactNode } from "react";
 import type { Tone } from "../../tokens/colors";
 
-export interface RadioGroupProps {
+interface RadioGroupRootProps {
   /**
-   * 라디오 버튼의 옵션 목록입니다.
-   * 2개 이상의 옵션을 제공해야 합니다.
+   * RadioGroup의 자식 요소들입니다.
+   * RadioGroup.Item 컴포넌트를 포함해야 합니다.
    */
-  options: {
-    label: ReactNode;
-    value: string;
-    /**
-     * @default false
-     */
-    disabled?: boolean;
-  }[];
+  children: ReactNode;
 
   /**
    * 폼 입력 이름: 동일한 그룹의 라디오 버튼들이 공유하는 이름을 지정합니다.
@@ -61,8 +54,45 @@ export interface RadioGroupProps {
   tone?: Tone;
 }
 
-// eslint-disable-next-line no-empty-pattern
-export function RadioGroup({}: RadioGroupProps) {
-  // TODO: RadioGroup 컴포넌트 구현
+/**
+ * RadioGroup의 루트 컴포넌트입니다.
+ * 라디오 버튼 그룹을 정의하고 상태를.관리합니다.
+ */
+// eslint-disable-next-line react-refresh/only-export-components, no-empty-pattern
+function Root({}: RadioGroupRootProps) {
+  // TODO: 구현
   return null;
 }
+
+interface RadioGroupItemProps {
+  /**
+   * 라디오 버튼의 값입니다.
+   */
+  value: string;
+
+  /**
+   * 라디오 버튼의 자식 요소입니다.
+   */
+  children?: ReactNode;
+
+  /**
+   * 비활성화 여부: true이면 이 라디오 버튼이 비활성화됩니다.
+   * @default false
+   */
+  disabled?: boolean;
+}
+
+/**
+ * 라디오 그룹의 개별 항목입니다.
+ * 선택 가능한 단일 라디오 버튼을 나타냅니다.
+ */
+// eslint-disable-next-line react-refresh/only-export-components, no-empty-pattern
+function Item({}: RadioGroupItemProps) {
+  // TODO: 구현
+  return null;
+}
+
+export const RadioGroup = {
+  Root,
+  Item,
+};
