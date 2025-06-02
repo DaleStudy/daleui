82bca9da5cb94f63637e0198dc2575d2c26f8d57
hyoseong1994 <wjdgytjd3291@gmail.com>
2025-06-03 00:05:18 +0900

    feat: spacing token re define

--- src/tokens/spacing.ts
+++ src/tokens/spacing.ts
@@ -3,63 +3,50 @@ import type { Tokens, SemanticTokens } from "@pandacss/types";
 export const semanticSpacing: SemanticTokens["spacing"] = {
   px: {
     sm: {
-      value: "{spacing.6}", // 1.5rem
+      value: "{spacing.24}", // 1.5rem
     },
     md: {
-      value: "{spacing.8}", // 2rem
+      value: "{spacing.32}", // 2rem
     },
     lg: {
-      value: "{spacing.10}", // 2.5rem
+      value: "{spacing.40}", // 2.5rem
     },
   },
   py: {
     sm: {
-      value: "{spacing.2}", // 0.5rem
+      value: "{spacing.8}", // 0.5rem
     },
     md: {
-      value: "{spacing.3}", // 0.75rem
+      value: "{spacing.12}", // 0.75rem
     },
     lg: {
-      value: "{spacing.4}", // 1rem
+      value: "{spacing.16}", // 1rem
     },
   },
   gap: {
     sm: {
-      value: "{spacing.4}", // 1rem
+      value: "{spacing.16}", // 1rem
     },
     md: {
-      value: "{spacing.6}", // 1.5rem
+      value: "{spacing.24}", // 1.5rem
     },
     lg: {
-      value: "{spacing.8}", // 2rem
+      value: "{spacing.32}", // 2rem
     },
   },
 };
 
 export const spacing: Tokens["spacing"] = {
   0: { value: "0rem" },
-  0.5: { value: "0.125rem" },
-  1: { value: "0.25rem" },
-  1.5: { value: "0.375rem" },
-  2: { value: "0.5rem" },
-  2.5: { value: "0.625rem" },
-  3: { value: "0.75rem" },
-  3.5: { value: "0.875rem" },
-  4: { value: "1rem" },
-  5: { value: "1.25rem" },
-  6: { value: "1.5rem" },
-  7: { value: "1.75rem" },
-  8: { value: "2rem" },
-  9: { value: "2.25rem" },
-  10: { value: "2.5rem" },
-  11: { value: "2.75rem" },
-  12: { value: "3rem" },
-  14: { value: "3.5rem" },
-  16: { value: "4rem" },
-  20: { value: "5rem" },
-  24: { value: "6rem" },
-  28: { value: "7rem" },
-  32: { value: "8rem" },
-  36: { value: "9rem" },
-  40: { value: "10rem" },
+  2: { value: "0.125rem" },
+  4: { value: "0.25rem" },
+  8: { value: "0.5rem" },
+  12: { value: "0.75rem" },
+  16: { value: "1rem" },
+  20: { value: "1.25rem" },
+  24: { value: "1.5rem" },
+  32: { value: "2rem" },
+  36: { value: "2.25rem" },
+  40: { value: "2.5rem" },
+  48: { value: "3rem" },
 };
