1d70edda94c20f7304694a07ad9ece990bbf3cfa
hyoseong1994 <wjdgytjd3291@gmail.com>
2025-06-10 22:18:48 +0900

    feat: underline definition by size and weight

--- src/components/Link/Link.stories.tsx
+++ src/components/Link/Link.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { vstack } from "../../../styled-system/patterns";
+import { hstack, vstack } from "../../../styled-system/patterns";
 import { Link } from "./Link";
 import { Icon } from "../Icon/Icon";
 
@@ -185,5 +185,262 @@ export const Inline: StoryObj<typeof Link> = {
         이 문장에는 <Link {...args}>링크</Link>가 포함되어 있습니다.
       </p>
     );
+  },
+};
+
+export const UnderlineThickness: StoryObj<typeof Link> = {
+  render: (args) => {
+    return (
+      <div className={vstack({ gap: "0", alignItems: "start" })}>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="2xs" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="2xs" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="2xs" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="2xs" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="2xs" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="xs" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="xs" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="xs" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="xs" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="xs" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="sm" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="sm" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="sm" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="sm" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="sm" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="md" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="md" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="md" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="md" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="md" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="lg" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="lg" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="lg" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="lg" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="lg" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="2xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="2xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="2xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="2xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="2xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="3xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="3xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="3xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="3xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="3xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="4xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="4xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="4xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="4xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="4xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="5xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="5xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="5xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="5xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="5xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="6xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="6xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="6xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="6xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="6xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="7xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="7xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="7xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="7xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="7xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="8xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="8xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="8xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="8xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="8xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+
+        <div className={hstack({ gap: "24" })}>
+          <Link {...args} size="9xl" weight="thin">
+            얇은
+          </Link>
+          <Link {...args} size="9xl" weight="light">
+            가벼운
+          </Link>
+          <Link {...args} size="9xl" weight="normal">
+            일반
+          </Link>
+          <Link {...args} size="9xl" weight="medium">
+            중간
+          </Link>
+          <Link {...args} size="9xl" weight="bold">
+            굵은
+          </Link>
+        </div>
+      </div>
+    );
+  },
+  argTypes: {
+    children: {
+      control: false,
+    },
+    weight: {
+      control: false,
+    },
   },
 };
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -24,25 +24,27 @@ interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
  * - `underline` 속성으로 밑줄 표시 여부를 설정할 수 있습니다. 기본값은 `true`입니다.
  * - `target="_blank"`를 사용할 때는 자동으로 보안 속성이 추가됩니다.
  * - `tone` 속성을 통해서 링크의 색상을 변경할 수 있습니다. 기본값은 `neutral`입니다.
- * - `size` 속성과 `weight` 속성을 통해서 텍스트 스타일을 변경할 수 있습니다.
+ * - `size` 속성을 통해서 텍스트 크기를 변경할 수 있습니다. 기본값으로 `md`를 사용합니다.
+ * - `weight` 속성을 통해서 텍스트 굵기를 변경할 수 있습니다. 기본값으로 `normal`을 사용합니다.
  * - `muted` 속성을 주시면 글자색이 옅어집니다. 명암비가 낮아지므로 접근성 측면에서 주의해서 사용하세요. 기본값은 `false`입니다.
  */
 export function Link({
   children,
   href,
   tone = "neutral",
-  size,
-  weight,
+  size = "md",
+  weight = "normal",
   muted = false,
   underline = true,
   target,
   rel,
   ...props
 }: LinkProps) {
+  const computedRel = target === "_blank" ? "noopener noreferrer" : rel;
   return (
     <a
       className={css(
-        styles.raw({ tone, muted, underline }),
+        style.raw({ tone, muted, underline, weight, size }),
         css.raw({
           fontSize: size,
           fontWeight: weight,
@@ -50,7 +52,7 @@ export function Link({
       )}
       href={href}
       target={target}
-      rel={target === "_blank" ? "noopener noreferrer" : rel}
+      rel={computedRel}
       {...props}
     >
       {children}
@@ -58,9 +60,9 @@ export function Link({
   );
 }
 
-const styles = cva({
+const style = cva({
   base: {
-    transition: "colors 0.2s",
+    transition: "colors 1s",
   },
   variants: {
     tone: {
@@ -76,6 +78,29 @@ const styles = cva({
     underline: {
       true: {},
       false: {},
+    },
+    weight: {
+      thin: {},
+      light: {},
+      normal: {},
+      medium: {},
+      bold: {},
+    },
+    size: {
+      "2xs": {},
+      xs: {},
+      sm: {},
+      md: {},
+      lg: {},
+      xl: {},
+      "2xl": {},
+      "3xl": {},
+      "4xl": {},
+      "5xl": {},
+      "6xl": {},
+      "7xl": {},
+      "8xl": {},
+      "9xl": {},
     },
   },
   compoundVariants: [
@@ -163,6 +188,7 @@ const styles = cva({
       underline: true,
       css: {
         textDecoration: "underline",
+        textDecorationColor: "currentColor",
       },
     },
     {
@@ -171,10 +197,431 @@ const styles = cva({
         textDecoration: "none",
       },
     },
+    {
+      underline: true,
+      size: "2xs",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "1.5px" },
+    },
+    {
+      underline: true,
+      size: "2xs",
+      weight: "light",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "1.5px" },
+    },
+    {
+      underline: true,
+      size: "2xs",
+      weight: "normal",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2px" },
+    },
+    {
+      underline: true,
+      size: "2xs",
+      weight: "medium",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2px" },
+    },
+    {
+      underline: true,
+      size: "2xs",
+      weight: "bold",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2.5px" },
+    },
+    {
+      underline: true,
+      size: "xs",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2px" },
+    },
+    {
+      underline: true,
+      size: "xs",
+      weight: "light",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2px" },
+    },
+    {
+      underline: true,
+      size: "xs",
+      weight: "normal",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2.5px" },
+    },
+    {
+      underline: true,
+      size: "xs",
+      weight: "medium",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2.5px" },
+    },
+    {
+      underline: true,
+      size: "xs",
+      weight: "bold",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "3px" },
+    },
+    {
+      underline: true,
+      size: "sm",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2px" },
+    },
+    {
+      underline: true,
+      size: "sm",
+      weight: "light",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2.5px" },
+    },
+    {
+      underline: true,
+      size: "sm",
+      weight: "normal",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "2.5px" },
+    },
+    {
+      underline: true,
+      size: "sm",
+      weight: "medium",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "3px" },
+    },
+    {
+      underline: true,
+      size: "sm",
+      weight: "bold",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "3px" },
+    },
+    {
+      underline: true,
+      size: "md",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "2.5px" },
+    },
+    {
+      underline: true,
+      size: "md",
+      weight: "light",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "3px" },
+    },
+    {
+      underline: true,
+      size: "md",
+      weight: "normal",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "3px" },
+    },
+    {
+      underline: true,
+      size: "md",
+      weight: "medium",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "3.5px" },
+    },
+    {
+      underline: true,
+      size: "md",
+      weight: "bold",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "4px" },
+    },
+    {
+      underline: true,
+      size: "lg",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "3px" },
+    },
+    {
+      underline: true,
+      size: "lg",
+      weight: "light",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "3.5px" },
+    },
+    {
+      underline: true,
+      size: "lg",
+      weight: "normal",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "4px" },
+    },
+    {
+      underline: true,
+      size: "lg",
+      weight: "medium",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "4px" },
+    },
+    {
+      underline: true,
+      size: "lg",
+      weight: "bold",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "4.5px" },
+    },
+    {
+      underline: true,
+      size: "xl",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "3.5px" },
+    },
+    {
+      underline: true,
+      size: "xl",
+      weight: "light",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "4px" },
+    },
+    {
+      underline: true,
+      size: "xl",
+      weight: "normal",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "4.5px" },
+    },
+    {
+      underline: true,
+      size: "xl",
+      weight: "medium",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "5px" },
+    },
+    {
+      underline: true,
+      size: "xl",
+      weight: "bold",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "5.5px" },
+    },
+    {
+      underline: true,
+      size: "2xl",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "4px" },
+    },
+    {
+      underline: true,
+      size: "2xl",
+      weight: "light",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "4.5px" },
+    },
+    {
+      underline: true,
+      size: "2xl",
+      weight: "normal",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "5px" },
+    },
+    {
+      underline: true,
+      size: "2xl",
+      weight: "medium",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "5.5px" },
+    },
+    {
+      underline: true,
+      size: "2xl",
+      weight: "bold",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "6px" },
+    },
+    {
+      underline: true,
+      size: "3xl",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "5px" },
+    },
+    {
+      underline: true,
+      size: "3xl",
+      weight: "light",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "5.5px" },
+    },
+    {
+      underline: true,
+      size: "3xl",
+      weight: "normal",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "6px" },
+    },
+    {
+      underline: true,
+      size: "3xl",
+      weight: "medium",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "6.5px" },
+    },
+    {
+      underline: true,
+      size: "3xl",
+      weight: "bold",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "7px" },
+    },
+    {
+      underline: true,
+      size: "4xl",
+      weight: "thin",
+      css: { textDecorationThickness: "1px", textUnderlineOffset: "6px" },
+    },
+    {
+      underline: true,
+      size: "4xl",
+      weight: "light",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "6.5px" },
+    },
+    {
+      underline: true,
+      size: "4xl",
+      weight: "normal",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "7px" },
+    },
+    {
+      underline: true,
+      size: "4xl",
+      weight: "medium",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "7.5px" },
+    },
+    {
+      underline: true,
+      size: "4xl",
+      weight: "bold",
+      css: { textDecorationThickness: "6px", textUnderlineOffset: "8px" },
+    },
+    {
+      underline: true,
+      size: "5xl",
+      weight: "thin",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "7px" },
+    },
+    {
+      underline: true,
+      size: "5xl",
+      weight: "light",
+      css: { textDecorationThickness: "2px", textUnderlineOffset: "7.5px" },
+    },
+    {
+      underline: true,
+      size: "5xl",
+      weight: "normal",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "8px" },
+    },
+    {
+      underline: true,
+      size: "5xl",
+      weight: "medium",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "8.5px" },
+    },
+    {
+      underline: true,
+      size: "5xl",
+      weight: "bold",
+      css: { textDecorationThickness: "6px", textUnderlineOffset: "9px" },
+    },
+    {
+      underline: true,
+      size: "6xl",
+      weight: "thin",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "8px" },
+    },
+    {
+      underline: true,
+      size: "6xl",
+      weight: "light",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "8.5px" },
+    },
+    {
+      underline: true,
+      size: "6xl",
+      weight: "normal",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "9px" },
+    },
+    {
+      underline: true,
+      size: "6xl",
+      weight: "medium",
+      css: { textDecorationThickness: "8px", textUnderlineOffset: "9.5px" },
+    },
+    {
+      underline: true,
+      size: "6xl",
+      weight: "bold",
+      css: { textDecorationThickness: "8px", textUnderlineOffset: "10px" },
+    },
+    {
+      underline: true,
+      size: "7xl",
+      weight: "thin",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "9px" },
+    },
+    {
+      underline: true,
+      size: "7xl",
+      weight: "light",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "9.5px" },
+    },
+    {
+      underline: true,
+      size: "7xl",
+      weight: "normal",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "10px" },
+    },
+    {
+      underline: true,
+      size: "7xl",
+      weight: "medium",
+      css: { textDecorationThickness: "8px", textUnderlineOffset: "11px" },
+    },
+    {
+      underline: true,
+      size: "7xl",
+      weight: "bold",
+      css: { textDecorationThickness: "8px", textUnderlineOffset: "11px" },
+    },
+    {
+      underline: true,
+      size: "8xl",
+      weight: "thin",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "10px" },
+    },
+    {
+      underline: true,
+      size: "8xl",
+      weight: "light",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "11px" },
+    },
+    {
+      underline: true,
+      size: "8xl",
+      weight: "normal",
+      css: { textDecorationThickness: "6px", textUnderlineOffset: "11px" },
+    },
+    {
+      underline: true,
+      size: "8xl",
+      weight: "medium",
+      css: { textDecorationThickness: "8px", textUnderlineOffset: "11.5px" },
+    },
+    {
+      underline: true,
+      size: "8xl",
+      weight: "bold",
+      css: { textDecorationThickness: "12px", textUnderlineOffset: "12px" },
+    },
+    {
+      underline: true,
+      size: "9xl",
+      weight: "thin",
+      css: { textDecorationThickness: "4px", textUnderlineOffset: "11px" },
+    },
+    {
+      underline: true,
+      size: "9xl",
+      weight: "light",
+      css: { textDecorationThickness: "8px", textUnderlineOffset: "11.5px" },
+    },
+    {
+      underline: true,
+      size: "9xl",
+      weight: "normal",
+      css: { textDecorationThickness: "12px", textUnderlineOffset: "12px" },
+    },
+    {
+      underline: true,
+      size: "9xl",
+      weight: "medium",
+      css: { textDecorationThickness: "12px", textUnderlineOffset: "12.5px" },
+    },
+    {
+      underline: true,
+      size: "9xl",
+      weight: "bold",
+      css: { textDecorationThickness: "16px", textUnderlineOffset: "13px" },
+    },
   ],
   defaultVariants: {
     tone: "neutral",
     muted: false,
     underline: true,
+    size: "md",
   },
 });
