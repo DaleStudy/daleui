a1d4a5b035dd6398611af80ac58008aaabacd370
SeonjaeLee <tjswo9502@gmail.com>
2025-06-25 23:15:32 +0900

    refactor: migrate import package

--- src/examples/login/Login.stories.tsx
+++ src/examples/login/Login.stories.tsx
@@ -1,4 +1,4 @@
-import { action } from "@storybook/addon-actions";
+import { action } from "storybook/actions";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "../../components/Button/Button";
 import { Checkbox } from "../../components/Checkbox/Checkbox";
