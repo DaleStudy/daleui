c7b7ba37915c6aa113b798260d30030009f14c5d
hsjeong <hsjeong@eivdnet.co.kr>
2025-06-26 10:15:28 +0900

    feat: link component re develop

--- src/components/Link/Link.test.tsx
+++ src/components/Link/Link.test.tsx
@@ -3,10 +3,9 @@ import { composeStories } from "@storybook/react-vite";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, test, vi } from "vitest";
-import { fontSizes, fontWeights } from "../../tokens/typography";
 import * as stories from "./Link.stories";
 
-const { Basic, Tones, Contrasts, Underlines, Security, WithIcon } =
+const { Basic, Tones, Underlines, Security, Icons, Sizes } =
   composeStories(stories);
 
 describe("렌더링 테스트", () => {
@@ -14,56 +13,32 @@ describe("렌더링 테스트", () => {
     render(<Basic />);
     expect(screen.getByRole("link")).toHaveTextContent("링크");
   });
-
-  test("Icon 컴포넌트와 함께 잘 사용한다", () => {
-    render(<WithIcon />);
-
-    const link = screen.getByRole("link");
-    expect(link).toHaveTextContent("링크");
-    expect(link).toContainElement(screen.getByLabelText("chevronRight"));
-  });
 });
 
 describe("스타일 테스트", () => {
   test.each([
     ["중립 링크", "c_fg.neutral"],
     ["브랜드 링크", "c_fg.brand"],
-    ["위험 링크", "c_fg.danger"],
-    // ["경고 링크", "c_fg.warning"],
-    ["성공 링크", "c_fg.success"],
-    ["정보 링크", "c_fg.info"],
   ] as const)("%s에 올바른 톤 클래스를 적용한다", (linkName, className) => {
     render(<Tones />);
 
     expect(screen.getByRole("link", { name: linkName })).toHaveClass(className);
   });
 
-  test("size prop에 따라 font size를 적용한다", () => {
-    const size = faker.helpers.arrayElement(
-      Object.keys(fontSizes),
-    ) as keyof typeof fontSizes;
-
-    render(<Basic size={size} />);
-    expect(screen.getByRole("link")).toHaveStyle({ fontSize: size });
-  });
-
-  test("weight prop에 따라 font weight을 적용한다", () => {
-    const weight = faker.helpers.arrayElement(
-      Object.keys(fontWeights),
-    ) as keyof typeof fontWeights;
-
-    render(<Basic weight={weight} />);
-    expect(screen.getByRole("link")).toHaveClass(`fw_${weight}`);
-  });
-
   test.each([
-    ["낮은 명암비", "c_fg.neutral.placeholder"],
-    ["높은 명암비", "c_fg.neutral"],
-  ] as const)("%s에 대해 올바른 클래스를 적용한다", (linkName, className) => {
-    render(<Contrasts />);
+    ["작은 링크", "fs_sm"],
+    ["중간 링크", "fs_md"],
+    ["큰 링크", "fs_lg"],
+  ] as const)(
+    "%s에 대해 올바른 font size를 적용한다",
+    (linkName, className) => {
+      render(<Sizes />);
 
-    expect(screen.getByRole("link", { name: linkName })).toHaveClass(className);
-  });
+      expect(screen.getByRole("link", { name: linkName })).toHaveClass(
+        className,
+      );
+    },
+  );
 
   test.each([
     ["밑줄 있음", "td_underline"],
@@ -78,6 +53,21 @@ describe("스타일 테스트", () => {
       );
     },
   );
+
+  test.each([
+    ["아이콘 있음", "i_externalLink"],
+    ["아이콘 없음", undefined],
+  ] as const)("%s에 올바른 아이콘 클래스를 적용한다", (linkName, className) => {
+    render(<Icons />);
+
+    const linkElement = screen.getByRole("link", { name: linkName });
+    const iconElement = linkElement.querySelector("svg");
+    if (className) {
+      expect(iconElement).toBeInTheDocument();
+    } else {
+      expect(iconElement).toBeNull();
+    }
+  });
 });
 
 describe("동작 테스트", () => {
@@ -112,7 +102,7 @@ describe("동작 테스트", () => {
   test("클릭 시, 해당 URL로 올바르게 이동한다", async () => {
     const user = userEvent.setup();
     const href = faker.internet.url({ appendSlash: true });
-    render(<Basic href={href} />);
+    render(<Basic href={href} onClick={undefined} />);
 
     const link = screen.getByRole("link");
     expect(link).toHaveAttribute("href", href);
@@ -146,7 +136,7 @@ describe("동작 테스트", () => {
   test("키보드로 link를 클릭 시, 해당 href로 올바르게 이동한다", async () => {
     const href = faker.internet.url({ appendSlash: true });
     const user = userEvent.setup();
-    render(<Basic href={href} />);
+    render(<Basic href={href} onClick={undefined} />);
 
     await user.tab();
     await user.keyboard("{Enter}");
