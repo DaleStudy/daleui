8d90618676598897b211617c1b4e7fde79eb299a
RiaOh <ken5840714@gmail.com>
2025-07-04 00:39:01 +0900

    feat: change test code

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -1,11 +1,9 @@
-import { faker } from "@faker-js/faker";
 import { composeStories } from "@storybook/react-vite";
 import { render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
-import { fontSizes, fontWeights } from "../../tokens/typography";
 import * as stories from "./Heading.stories";
 
-const { Basic, Contrasts } = composeStories(stories);
+const { Basic, Tones } = composeStories(stories);
 
 test("텍스트와 함께 heading이 올바르게 렌더링됨", () => {
   render(<Basic>제목</Basic>);
@@ -22,34 +20,19 @@ test.each([1, 2, 3, 4, 5] as const)(
   },
 );
 
-test("weight prop에 따라 font weight 클래스가 올바르게 적용됨", () => {
-  const weight = faker.helpers.arrayElement(
-    Object.keys(fontWeights),
-  ) as keyof typeof fontWeights;
+test.each(["md", "lg", "xl", "2xl", "3xl"] as const)(
+  "size prop에 따라 font size 클래스가 올바르게 적용됨",
+  (size) => {
+    render(<Basic size={size} />);
+    expect(screen.getByRole("heading")).toHaveClass(`fs_${size}`);
+  },
+);
 
-  render(<Basic weight={weight} />);
+test.each([
+  ["중립 색조", "c_fg.neutral"],
+  ["브랜드 색조", "c_fg.brand"],
+] as const)("%s에 올바른 톤 클래스를 적용한다", (textName, className) => {
+  render(<Tones />);
 
-  expect(screen.getByRole("heading")).toHaveClass(`fw_${weight}`);
-});
-
-test("size prop에 따라 font size 클래스가 올바르게 적용됨", () => {
-  const size = faker.helpers.arrayElement(
-    Object.keys(fontSizes),
-  ) as keyof typeof fontSizes;
-
-  render(<Basic size={size} />);
-
-  expect(screen.getByRole("heading")).toHaveClass(`fs_${size}`);
-});
-
-test("낮은 명암비와 높은 명암비에 따라 글자 색이 올바르게 적용됨", () => {
-  render(<Contrasts />);
-
-  expect(screen.getByRole("heading", { name: "낮은 명암비" })).toHaveClass(
-    "c_fg.neutral.placeholder",
-  );
-
-  expect(screen.getByRole("heading", { name: "높은 명암비" })).toHaveClass(
-    "c_fg.neutral",
-  );
+  expect(screen.getByText(textName)).toHaveClass(className);
 });
