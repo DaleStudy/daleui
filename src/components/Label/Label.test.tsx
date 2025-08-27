9e58817f7df4f19238f2bcced7efa3a91195dd4a
RiaOh <ken5840714@gmail.com>
2025-08-27 23:25:47 +0900

    chore: change test code

--- src/components/Label/Label.test.tsx
+++ src/components/Label/Label.test.tsx
@@ -13,27 +13,34 @@ test("텍스트와 함께 라벨이 올바르게 렌더링됨", () => {
 
 test("tone 속성이 올바르게 적용됨", () => {
   render(<Tones />);
-  expect(screen.getAllByText("중립 색조 라벨")[0]).toHaveClass(
-    "c_fg.neutral.DEFAULT",
-  );
-  expect(screen.getAllByText("위험 색조 라벨")[0]).toHaveClass("c_fg.danger");
+  const neutralText = screen.getAllByText("중립 색조 라벨")[0];
+  const dangerText = screen.getAllByText("위험 색조 라벨")[0];
+  // eslint-disable-next-line testing-library/no-node-access
+  expect(neutralText.closest("label")).toHaveClass("c_fg.neutral.DEFAULT");
+  // eslint-disable-next-line testing-library/no-node-access
+  expect(dangerText.closest("label")).toHaveClass("c_fg.danger");
 });
 
 test("disabled 속성이 올바르게 적용됨", () => {
   render(<Disabled />);
-  expect(screen.getByText("비활성화 라벨")).toHaveClass(
-    "c_fg.neutral.disabled",
-  );
+  const disabledText = screen.getByText("비활성화 라벨");
+  // eslint-disable-next-line testing-library/no-node-access
+  expect(disabledText.closest("label")).toHaveClass("c_fg.neutral.disabled");
 });
 
 test("variant 속성이 올바르게 적용됨", () => {
   render(<Variants />);
   expect(screen.getByText("기본 라벨")).toBeInTheDocument();
 
-  const requiredLabel = screen.getByText("필수 라벨");
-  expect(within(requiredLabel).getByText("*")).toHaveClass("c_fg.danger");
+  const requiredText = screen.getByText("필수 라벨");
+  // eslint-disable-next-line testing-library/no-node-access
+  const requiredLabel = requiredText.closest("label") as HTMLElement;
+  const asterisk = within(requiredLabel).getByLabelText("옵션 필수");
+  expect(asterisk).toHaveClass("c_fg.danger");
 
-  const optionalLabel = screen.getByText("선택 라벨");
+  const optionalText = screen.getByText("선택 라벨");
+  // eslint-disable-next-line testing-library/no-node-access
+  const optionalLabel = optionalText.closest("label") as HTMLElement;
   expect(optionalLabel).toHaveTextContent(/\(옵션 선택\)/);
 });
 
