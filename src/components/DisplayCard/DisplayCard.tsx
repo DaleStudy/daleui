5bb8ed5c0ffc22e59245ab896f245accf6c2d5da
HowToBeAHappyBoy <dev@seozi.io>
2025-09-07 16:57:20 +0900

    feat: Add DisplayCard Component

---
+++ src/components/DisplayCard/DisplayCard.tsx
@@ -0,0 +1,114 @@
+import type { Tone } from "../../tokens/colors";
+import type { IconName } from "../../tokens/iconography";
+import { Icon } from "../Icon/Icon";
+import { Link } from "../Link/Link";
+import { cva, cx } from "../../../styled-system/css";
+import { stack } from "../../../styled-system/patterns";
+import { Text } from "../Text/Text";
+
+type BaseDisplayCardProps = {
+  /** 제목 */
+  title: string;
+  /** 설명 */
+  description: string;
+  /** 아이콘 */
+  icon: IconName;
+  /** 색조 */
+  tone?: Extract<Tone, "neutral" | "brand">;
+  /** border 여부 */
+  outline?: boolean;
+  /** 클래스명 */
+  className?: string;
+};
+
+type DisplayCardPropsWithLink = BaseDisplayCardProps & {
+  link: string;
+  linkText: string;
+  isExternalLink?: boolean;
+};
+
+type DisplayCardPropsWithoutLink = BaseDisplayCardProps & {
+  link?: undefined;
+  linkText?: undefined;
+  isExternalLink?: undefined;
+};
+
+type DisplayCardProps = DisplayCardPropsWithLink | DisplayCardPropsWithoutLink;
+
+export function DisplayCard({
+  title,
+  description,
+  icon,
+  tone = "neutral",
+  link,
+  linkText,
+  isExternalLink,
+  outline = false,
+  className,
+}: DisplayCardProps) {
+  return (
+    <article className={cx(styles({ tone, outline }), className)}>
+      <div
+        className={stack({
+          display: "inline-flex",
+          p: "12",
+          borderRadius: "md",
+          bg: "bg.brand",
+        })}
+      >
+        <Icon name={icon} size="lg" tone={tone} />
+      </div>
+      <div className={stack({ gap: "4" })}>
+        <Text size="lg" weight="semibold" as="p">
+          {title}
+        </Text>
+        <Text size="md" as="p">
+          {description}
+        </Text>
+      </div>
+      {link && (
+        <Link
+          href={link}
+          size="lg"
+          tone={tone}
+          underline={false}
+          external={isExternalLink}
+        >
+          {linkText}
+          <Icon name="externalLink" tone={tone} size="sm" />
+        </Link>
+      )}
+    </article>
+  );
+}
+
+const styles = cva({
+  base: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+    gap: "24",
+    p: "24",
+    px: "16",
+    borderRadius: "md",
+    bg: "bg.neutral",
+  },
+  variants: {
+    outline: {
+      true: {
+        borderWidth: "sm",
+      },
+      false: {
+        borderWidth: "0",
+      },
+    },
+    tone: {
+      neutral: {
+        borderColor: "border.neutral",
+      },
+      brand: {
+        borderColor: "border.brand",
+      },
+    },
+  },
+});
