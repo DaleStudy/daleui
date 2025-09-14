beb3a598c09d7446ef79c7244da7c6393b3fe535
RiaOh <ken5840714@gmail.com>
2025-09-14 20:43:51 +0900

    feat: add and refactor Navigation Section code

--- src/marketing/NavigationSection.tsx
+++ src/marketing/NavigationSection.tsx
@@ -7,145 +7,206 @@ import { useState } from "react";
 
 export function NavigationSection() {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const handleToggleTheme = () => {
+    const el = document.documentElement;
+    const next = el.getAttribute("data-theme") === "dark" ? "light" : "dark";
+    el.setAttribute("data-theme", next);
+    el.classList.toggle("dark", next === "dark");
+  };
+  const handleToggleMenu = () => {
+    setIsOpenMenu((v) => !v);
+  };
+  const handleSponsorClick = () => {
+    window.open("https://github.com/sponsors/DaleStudy", "_blank");
+  };
   return (
-    <nav
-      className={stack({
-        direction: { base: "column", md: "row" },
-        width: {
-          base: "100%",
-        },
-        maxWidth: { base: "100%", md: "1280px" },
-        px: { base: "0", md: "24" },
-        py: { base: "0", md: "5px" },
-        justifyContent: "space-between",
-        alignItems: { base: "flex-start", md: "center" },
-        position: "relative",
-        gap: { base: "0" },
+    <header
+      className={css({
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       })}
     >
-      <div
-        className={css({
-          width: { base: "100%", md: "auto" },
-          height: "71px",
-          px: { base: 16 },
-          display: "flex",
-          alignItems: "center",
-        })}
-      >
-        <Link href="/" underline={false}>
-          <img
-            src="/logo.svg"
-            alt="DaleUI Logo"
-            className={css({
-              height: { base: "1.5rem", md: "2rem" },
-              objectFit: "contain",
-              marginRight: "10px",
-            })}
-          />
-          <div
-            className={css({
-              color: "fg.brand",
-              fontFamily: "Roboto",
-              fontSize: "2xl",
-              fontStyle: "normal",
-              fontWeight: "bold",
-              lineHeight: "tight",
-            })}
-          >
-            Dale UI
-          </div>
-        </Link>
-        <span
-          className={css({
-            display: { base: "inline-flex", md: "none" },
-            position: "absolute",
-            top: "16",
-            right: "16",
-          })}
-        >
-          <Icon
-            tone="brand"
-            name={isOpenMenu ? "x" : "menu"}
-            size="lg"
-            onClick={() => setIsOpenMenu((v) => !v)}
-          />
-        </span>
-      </div>
-
-      <ul
+      <nav
         className={stack({
-          gap: { base: "24", md: "45" },
-          display: { base: isOpenMenu ? "flex" : "none", md: "flex" },
           direction: { base: "column", md: "row" },
+          maxWidth: "1024px",
+          width: {
+            base: "100%",
+          },
+          px: { base: "16", md: "24" },
+          py: "5px",
+          pb: { lg: "6px" },
+          justifyContent: "space-between",
           alignItems: { base: "flex-start", md: "center" },
-          width: { base: "100%", md: "auto" },
-          paddingLeft: { base: "24", md: "0" },
-          paddingRight: { base: "24", md: "0" },
-          paddingTop: { base: "24", md: "0" },
+          position: "relative",
+          gap: { base: "0" },
         })}
       >
-        <li>
-          <Link
-            href="https://github.com/DaleStudy/daleui"
-            external={true}
-            underline={false}
-            tone="neutral"
-            size="lg"
-          >
-            깃허브
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://main--675790d317ba346348aa3490.chromatic.com/"
-            external={true}
-            underline={false}
-            tone="neutral"
-            size="lg"
-          >
-            스토리북
-          </Link>
-        </li>
-        <li>
-          <Link href="/" underline={false} tone="neutral" size="lg">
-            피그마 디자인
-          </Link>
-        </li>
-      </ul>
-
-      <ul
-        className={stack({
-          gap: "22",
-          display: { base: isOpenMenu ? "flex" : "none", md: "flex" },
-          direction: { base: "column", md: "row" },
-          alignItems: { base: "flex-start", md: "center" },
-          width: { base: "100%", md: "auto" },
-          padding: { base: "24", md: "0" },
-        })}
-      >
-        <li>
-          <Icon
-            name="sun"
-            size="lg"
-            tone="brand"
-            onClick={() => {
-              const el = document.documentElement;
-              const next =
-                el.getAttribute("data-theme") === "dark" ? "light" : "dark";
-              el.setAttribute("data-theme", next);
-              el.classList.toggle("dark", next === "dark");
-            }}
-          />
-        </li>
-        <li
+        {/* 로고 & 햄버거바 */}
+        <div
           className={css({
             width: { base: "100%", md: "auto" },
-            "& > button": { width: { base: "100%", md: "auto" } },
+            justifyContent: "space-between",
+            display: "flex",
+            alignItems: "center",
           })}
         >
-          <Button variant="solid">후원하기</Button>
-        </li>
-      </ul>
-    </nav>
+          {/* Nav Logo */}
+          <div
+            className={css({
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "space-between",
+              width: "150px",
+              padding: "10px",
+              gap: "10px",
+              flexDirection: "column",
+            })}
+          >
+            {/* Logo */}
+            <div
+              className={css({
+                display: "flex",
+                alignItems: "center",
+                gap: "10px",
+                px: "3px",
+                py: "6px",
+              })}
+            >
+              <img
+                src="/newLogo.svg"
+                alt="DaleUI Logo"
+                className={css({
+                  height: "24px",
+                  width: "30px",
+                  objectFit: "contain",
+                })}
+              />
+              <div
+                className={css({
+                  color: "fg.brand",
+                  fontFamily: "Roboto",
+                  fontSize: "2xl",
+                  fontStyle: "normal",
+                  fontWeight: "bold",
+                  lineHeight: "tight",
+                })}
+              >
+                Dale UI
+              </div>
+            </div>
+          </div>
+
+          {/* 햄버거바 */}
+          <div
+            className={css({
+              display: { base: "inline-flex", md: "none" },
+              top: "24",
+              right: "24",
+              padding: "10px",
+            })}
+          >
+            <Icon
+              tone="brand"
+              name={isOpenMenu ? "x" : "menu"}
+              size="lg"
+              onClick={handleToggleMenu}
+            />
+          </div>
+        </div>
+
+        {/* 링크 */}
+        <ul
+          className={stack({
+            gap: { base: "24", md: "45" },
+            display: "flex",
+            direction: { base: "column", md: "row" },
+            alignItems: { base: "flex-start", md: "center" },
+            width: { base: "100%", md: "auto" },
+            paddingLeft: { base: "24", md: "0" },
+            paddingRight: { base: "24", md: "0" },
+            paddingTop: { base: "24", md: "0" },
+            opacity: { base: isOpenMenu ? 1 : 0, md: 1 },
+            transform: {
+              base: isOpenMenu ? "translateY(0)" : "translateY(-8px)",
+              md: "none",
+            },
+            pointerEvents: { base: isOpenMenu ? "auto" : "none", md: "auto" },
+            transition: "opacity 200ms ease, transform 200ms ease",
+          })}
+        >
+          <li>
+            <Link
+              href="https://github.com/DaleStudy/daleui"
+              external={true}
+              underline={false}
+              tone="neutral"
+              size="lg"
+            >
+              깃허브
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://main--675790d317ba346348aa3490.chromatic.com/"
+              external={true}
+              underline={false}
+              tone="neutral"
+              size="lg"
+            >
+              스토리북
+            </Link>
+          </li>
+          <li>
+            <Link href="/" underline={false} tone="neutral" size="lg">
+              피그마 디자인
+            </Link>
+          </li>
+        </ul>
+
+        {/* 토글 & 후원하기 */}
+        <ul
+          className={stack({
+            gap: "22",
+            display: "flex",
+            direction: { base: "column", md: "row" },
+            alignItems: { base: "flex-start", md: "center" },
+            width: { base: "100%", md: "auto" },
+            padding: { base: "24", md: "0" },
+            opacity: { base: isOpenMenu ? 1 : 0, md: 1 },
+            transform: {
+              base: isOpenMenu ? "translateY(0)" : "translateY(-8px)",
+              md: "none",
+            },
+            pointerEvents: { base: isOpenMenu ? "auto" : "none", md: "auto" },
+            transition: "opacity 200ms ease, transform 200ms ease",
+          })}
+        >
+          <li>
+            <Icon
+              name="sun"
+              size="lg"
+              tone="brand"
+              onClick={handleToggleTheme}
+              className={css({
+                cursor: "pointer",
+              })}
+            />
+          </li>
+          <li
+            className={css({
+              width: { base: "100%", md: "auto" },
+              "& > button": { width: { base: "100%", md: "auto" } },
+            })}
+          >
+            <Button variant="solid" onClick={handleSponsorClick}>
+              후원하기
+            </Button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
