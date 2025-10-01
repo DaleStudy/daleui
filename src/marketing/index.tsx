ef33a398767ed3c89f80e2054b96252fe518a441
Hecklebot <sohansaem@gmail.com>
2025-10-01 22:53:16 +0900

    wip: integrate marketing page

---
+++ src/marketing/index.tsx
@@ -0,0 +1,22 @@
+import { Navigation } from "./Navigation";
+import { Header } from "./Header";
+import { How } from "./How";
+import { Community } from "./Community";
+import { Contribution } from "./Contribution";
+
+/**
+ * DaleUI 마케팅 페이지
+ *
+ * Navigation, Header, How, Community, Contribution 섹션으로 구성됩니다.
+ */
+export function MarketingPage() {
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <How />
+      <Community />
+      <Contribution />
+    </>
+  );
+}
