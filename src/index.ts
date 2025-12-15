bbc281e7fa3a7f7a6205acf55cf3d4892410c103
hyoseong1994 <wjdgytjd3291@gmail.com>
2025-12-15 10:09:27 +0900

    feat: export type and create entry point

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+export { Button, type ButtonProps } from "./components/Button/Button";
+export { Card } from "./components/Card/Card";
+export { Checkbox, type CheckboxProps } from "./components/Checkbox/Checkbox";
+export { Flex, type FlexProps } from "./components/Flex/Flex";
+export { Heading, type HeadingProps } from "./components/Heading/Heading";
+export { HStack, type HStackProps } from "./components/HStack/HStack";
+export { Icon, type IconProps } from "./components/Icon/Icon";
+export { Label, type LabelProps } from "./components/Label/Label";
+export { Link, type LinkProps } from "./components/Link/Link";
+export {
+  PasswordInput,
+  type PasswordInputProps,
+} from "./components/PasswordInput/PasswordInput";
+export {
+  RadioGroup,
+  type RadioGroupProps,
+} from "./components/RadioGroup/RadioGroup";
+export { Select, type SelectProps } from "./components/Select/Select";
+export { Tag, type TagProps } from "./components/Tag/Tag";
+export { Text, type TextProps } from "./components/Text/Text";
+export {
+  TextInput,
+  type TextInputProps,
+} from "./components/TextInput/TextInput";
+export { VStack, type VStackProps } from "./components/VStack/VStack";
