e0a9e5f21856f365acf261259296c2c4d01289ce
hyoseong1994 <wjdgytjd3291@gmail.com>
2025-12-17 08:50:32 +0900

    test: bundle badge

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "daleui-bundle",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   test: {
     environment: "happy-dom",
     setupFiles: ["./src/setupTests.tsx"],
