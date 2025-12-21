8d232f066f02944ca29c3e28a5cfed5748f80e4c
hyoseong1994 <wjdgytjd3291@gmail.com>
2025-12-21 21:14:08 +0900

    chore: update module exports

--- vite.config.ts
+++ vite.config.ts
@@ -27,20 +27,25 @@ export default defineConfig({
   },
   build: {
     lib: {
+      // 라이브러리 진입점 설정합니다.
       entry: {
+        // import { Button } from "daleui";
         index: resolve(__dirname, "src/index.ts"),
+        // import { Button } from "daleui/button";
         ...componentEntries,
       },
-      formats: ["es", "cjs"],
-      fileName: (format, entryName) =>
-        format === "es" ? `${entryName}.mjs` : `${entryName}.cjs`,
+      formats: ["es"],
     },
+
     rollupOptions: {
+      // 사용하는 쪽 프로젝트의 react를 그대로 사용하게 합니다.
       external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
+        // 모든 CSS 자산을 하나의 파일로 만듭니다. (PandaCSS는 빌드타임에 css를 생성하기 떄문에)
         assetFileNames: "index.css",
       },
     },
+    // public 디렉토리를 빌드 결과물에 복사하지 않습니다.
     copyPublicDir: false,
   },
 });
