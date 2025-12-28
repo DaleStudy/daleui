892448271a73458186a34a2810048a5ffb9bc2ad
hyoseong1994 <wjdgytjd3291@gmail.com>
2025-12-28 18:14:26 +0900

    chore: re-commit by publish setup

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -5,7 +5,7 @@ import { textStyles } from "../../tokens/typography";
 type LinkSize = "sm" | "md" | "lg";
 type LinkTone = "neutral" | "brand";
 
-interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+export interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   /** 링크 URL (필수) */
   href: string;
   /** 링크 내용, 아이콘, 이미지 등 (필수) */
