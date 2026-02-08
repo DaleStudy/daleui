78b477f32535a4a14e3c1a2af64e90d58ddf6821
RiaOh <ken5840714@gmail.com>
2026-02-08 20:26:15 +0900

    feat: add Controlled/Uncontrolled mode documentation and tests of Select Component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -20,9 +20,11 @@ export interface SelectProps extends HTMLAttributes<HTMLSelectElement> {
   defaultValue?: string;
 
   /** native props */
-  /** 선택된 값 */
+  /** 선택된 값 (controlled 모드) */
   value?: string;
-  /** 값이 변경될 때 호출되는 함수 */
+  /** 초기 선택값 (uncontrolled 모드) */
+  defaultValue?: string;
+  /** 값이 변경될 때 호출되는 함수 (controlled 모드) */
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   /** 비활성화 상태 */
   disabled?: boolean;
