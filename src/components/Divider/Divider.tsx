1ad51875f08385e44a8f7121b4bdad623185e202
yolophg <contact.helena.park@gmail.com>
2026-07-29 00:06:50 -0400

    refactor: tighten Divider props and add docs from review feedback

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,7 +1,10 @@
 import type { HTMLAttributes, Ref } from "react";
 import { cva, cx } from "../../../styled-system/css";
 
-export interface DividerProps extends HTMLAttributes<HTMLElement> {
+export interface DividerProps extends Omit<
+  HTMLAttributes<HTMLElement>,
+  "children" | "dangerouslySetInnerHTML"
+> {
   /** 방향 */
   orientation?: "horizontal" | "vertical";
   /** 선 스타일 */
@@ -16,6 +19,7 @@ export interface DividerProps extends HTMLAttributes<HTMLElement> {
  * Divider는 콘텐츠를 시각적으로 구분하는 선입니다.
  *
  * - `orientation="horizontal"`(기본값)은 세로로 쌓인 콘텐츠를 가로 선으로 구분할 때, `orientation="vertical"`은 가로로 나열된 콘텐츠를 세로 선으로 구분할 때 사용합니다.
+ * - `orientation="vertical"`은 높이를 `align-self: stretch`로 채우므로 부모가 flex 또는 grid 컨테이너여야 합니다. 그 외의 부모에서는 `align-self`가 무시되어 높이가 0이 되므로, `height` 등으로 직접 높이를 지정해야 합니다.
  */
 export function Divider({
   ref,
